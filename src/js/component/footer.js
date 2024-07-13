5fa215752f1347354dda5704b1dca6c439c6d5ba
Usman257 <117535587+Usman257@users.noreply.github.com>
2024-07-13 12:17:44 +0000

    terminado

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,35 +1,10 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
+import React, { Component } from "react";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
-
-//create your first component
-const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
-
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
-};
-
-export default injectContext(Layout);
+export const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
+			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
+	</footer>
+);
